fbededefbf05344f89be1edfdd764398828a2d76
Fred <fpaula@gmail.com>
2012-05-08 17:37:35 -0300

    Working with white spaces

--- text_parser.js
+++ text_parser.js
@@ -1,39 +1,52 @@
+var find_in_array,
+    prepare_text,
+    process_dictionary,
+    match_text;
+
 exports.parse = function(text, args) {
-  var result = [];
+  var result,
+      dictionary,
+      item,
+      word,
+      matches,
+      item_index;
+
+  result = [];
   text = prepare_text(text);
-  var dictionary = process_dictionary(text, args);
-  for (var item in dictionary) {
-    if (dictionary[item] == '') continue;
-    var matches = match_text(text, dictionary[item]);
+  dictionary = process_dictionary(text, args);
+  for (item in dictionary) {
+    word = prepare_text(dictionary[item]);
+    if (word === '') continue;
+    matches = match_text(text, word);
     if (matches != null) {
-      var item_index = find_in_array(result, dictionary[item]);
-      if (item_index == null) {
-        result.push({'word': dictionary[item], 'hits': matches.length});
+      item_index = find_in_array(result, word);
+      if (item_index === null) {
+        result.push({'word': word, 'hits': matches.length});
       }
     }
   }
   return result;
 }
 
-function find_in_array(array, term){
+find_in_array = function(array, word){
   for (var item in array) {
-    if (array[item]['word'] == term) return item;
+    if (array[item]['word'] === word) return item;
   }
   return null
 }
 
-function prepare_text(text) {
+prepare_text = function(text) {
   text = text.replace(/[^\w\s]/gi,' ')
   text = text.replace(/^\s+|\s+$/g,"");
   return text.toLowerCase();
 }
 
-function process_dictionary(text, args) {
-  if (args['dictionary'] == undefined) return text.split(' ');
+process_dictionary = function(text, args) {
+  if (typeof args['dictionary'] === 'undefined') return text.split(' ');
   return args['dictionary'];
 }
 
-function match_text(text, word) {
-  var r = new RegExp('\\b' + word + '\\b', 'g');
+match_text = function(text, word) {
+  var r = new RegExp('\\b' + word + '\\b', 'gi');
   return text.match(r);
 }
